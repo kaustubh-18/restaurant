d414ce2fa00db005b47c4989e75188ad5e8b878f
GirishACTS <GirishACTS@DESKTOP-8TNE2BE>
2019-08-04 18:08:18 +0530

    spring security done

--- Restaurant/src/main/java/com/sample/Restaurant/RestaurantApplication.java
+++ Restaurant/src/main/java/com/sample/Restaurant/RestaurantApplication.java
@@ -1,7 +1,14 @@
 package com.sample.Restaurant;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RestaurantApplication {
@@ -10,4 +17,16 @@ public class RestaurantApplication {
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	public DataSource dataSource() {
+		 return DataSourceBuilder
+		        .create()
+		        .build();
+	}
 }
--- Restaurant/src/main/java/com/sample/Restaurant/SecurityConfiguration/SecurityConfigurations.java
+++ Restaurant/src/main/java/com/sample/Restaurant/SecurityConfiguration/SecurityConfigurations.java
@@ -1,28 +1,43 @@
 package com.sample.Restaurant.SecurityConfiguration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	
 	/* (non-Javadoc)
 	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder)
 	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 
 
-	/* (non-Javadoc)
+	/* (non-Javadoc) 
 	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
 	 */
 	@Override
@@ -34,7 +49,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		.defaultSuccessUrl("/welcome",true)
 		.and().logout().permitAll();
 		
-		
 	}
-
+	
+	
 }
